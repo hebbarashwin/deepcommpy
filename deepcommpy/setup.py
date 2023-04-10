81d57bb13502b2689d991749a502a6a7371e2274
Ashwin Hebbar <hebbarashwin@gmail.com>
2023-04-09 20:06:22 -0400

    init

--- deepcommpy/setup.py
+++ deepcommpy/setup.py
@@ -5,20 +5,19 @@ setup(
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
-        matplotlib==3.5.1,
-        numpy==1.21.5,
-        tqdm==4.62.3,
-        six==1.16.0,
-        torch==1.10.2
+        'matplotlib==3.5.1',
+        'numpy==1.21.5',
+        'six==1.16.0',
+        'torch==1.10.2',
+        'tqdm==4.62.3'
     ],
     extras_require={
         'matlab': ['matlab'],  # Add optional MATLAB dependency
     },
     entry_points={
         'console_scripts': [
-            'tinyturbo-train=deepcommpy.tinyturbo.trainer:main',
-            'tinyturbo-decode=deepcommpy.tinyturbo.decoder:main',
-            # Add other entry points for crisp and deepcode if necessary
+            'tinyturbo-train=deepcommpy.tinyturbo.tinyturbo:main',
+            # Add other entry points for crisp and nams if necessary
         ],
     },
 )
