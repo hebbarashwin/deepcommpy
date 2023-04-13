0faa5f5db98bb364e50b3a9e47b57aa0ce013964
Ashwin Hebbar <hebbarashwin@gmail.com>
2023-04-13 14:54:37 -0400

    added config

--- deepcommpy/crisp/crisp_script.py
+++ deepcommpy/crisp/crisp_script.py
@@ -37,7 +37,7 @@ def crisp_rnn_test(polar, device, net=None, config=None):
 
     with torch.no_grad():
         for k in range(num_test_batches):
-            msg_bits = 2*torch.randint(0, 2, (config['test_batch_size'], config['K']), dtype=torch.float) - 1
+            msg_bits = 2*torch.randint(0, 2, (config['test_batch_size'], polar.K), dtype=torch.float) - 1
             msg_bits = msg_bits.to(device)
             polar_code = polar.encode(msg_bits)
             for snr_ind, snr in enumerate(snr_range):
@@ -45,14 +45,14 @@ def crisp_rnn_test(polar, device, net=None, config=None):
                 noisy_code = channel.corrupt_signal(polar_code, sigma, vv = config['vv'], radar_power = config['radar_power'], radar_prob = config['radar_prob'])
                 noise = noisy_code - polar_code
 
-                SC_llrs, decoded_SC_msg_bits = polar.sc_decode_new(noisy_code, snr)
-                ber_SC = errors_ber(msg_bits, decoded_SC_msg_bits.sign()).item()
-                bler_SC = errors_bler(msg_bits, decoded_SC_msg_bits.sign()).item()
+                SC_llrs, decoded_SC_msg_bits = polar.sc_decode(noisy_code, snr)
+                ber_SC = errors_ber(msg_bits, decoded_SC_msg_bits.sign())
+                bler_SC = errors_bler(msg_bits, decoded_SC_msg_bits.sign())
 
                 decoded_RNN_msg_bits = polar.crisp_rnn_decode(noisy_code, net)
 
-                ber_RNN = errors_ber(msg_bits, decoded_RNN_msg_bits.sign()).item()
-                bler_RNN = errors_bler(msg_bits, decoded_RNN_msg_bits.sign()).item()
+                ber_RNN = errors_ber(msg_bits, decoded_RNN_msg_bits.sign())
+                bler_RNN = errors_bler(msg_bits, decoded_RNN_msg_bits.sign())
 
 
                 bers_RNN_test[snr_ind] += ber_RNN/num_test_batches
@@ -85,7 +85,7 @@ def crisp_cnn_test(polar, device, net=None, config=None):
 
     with torch.no_grad():
         for k in range(num_test_batches):
-            msg_bits = 2*torch.randint(0, 2, (config['test_batch_size'], config['K']), dtype=torch.float) - 1
+            msg_bits = 2*torch.randint(0, 2, (config['test_batch_size'], polar.K), dtype=torch.float) - 1
             msg_bits = msg_bits.to(device)
             polar_code = polar.encode(msg_bits)
             for snr_ind, snr in enumerate(snr_range):
@@ -93,14 +93,14 @@ def crisp_cnn_test(polar, device, net=None, config=None):
                 noisy_code = channel.corrupt_signal(polar_code, sigma, vv = config['vv'], radar_power = config['radar_power'], radar_prob = config['radar_prob'])
                 noise = noisy_code - polar_code
 
-                SC_llrs, decoded_SC_msg_bits = polar.sc_decode_new(noisy_code, snr)
-                ber_SC = errors_ber(msg_bits, decoded_SC_msg_bits.sign()).item()
-                bler_SC = errors_bler(msg_bits, decoded_SC_msg_bits.sign()).item()
+                SC_llrs, decoded_SC_msg_bits = polar.sc_decode(noisy_code, snr)
+                ber_SC = errors_ber(msg_bits, decoded_SC_msg_bits.sign())
+                bler_SC = errors_bler(msg_bits, decoded_SC_msg_bits.sign())
 
                 decoded_CNN_msg_bits = polar.crisp_cnn_decode(noisy_code, net)
 
-                ber_CNN = errors_ber(msg_bits, decoded_CNN_msg_bits.sign()).item()
-                bler_CNN = errors_bler(msg_bits, decoded_CNN_msg_bits.sign()).item()
+                ber_CNN = errors_ber(msg_bits, decoded_CNN_msg_bits.sign())
+                bler_CNN = errors_bler(msg_bits, decoded_CNN_msg_bits.sign())
 
 
                 bers_CNN_test[snr_ind] += ber_CNN/num_test_batches
@@ -112,5 +112,3 @@ def crisp_cnn_test(polar, device, net=None, config=None):
 
     return bers_CNN_test, blers_CNN_test, bers_SC_test, blers_SC_test
 
-def main(config, gpu = -1):
-    pass
